b128a4d0f6926cda158a9bb8bd4cdeaa53dc95a4
Peacock <me@p3ac0ck.net>
2024-07-22 18:09:33 +0900

    :wrench: Use nix flakes with nix-direnv

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "slidev-slides";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+        flakedPkgs = pkgs;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20 corepack_20
+          ];
+        };
+      }
+    );
+}
