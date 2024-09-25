049cf39c4d620cc14ffed170399d29aa6b014f93
Peacock <me@p3ac0ck.net>
2024-09-26 03:35:59 +0900

    :tada: Initlize for the PyCon JP 2024

---
+++ talks/2024-09-27_pyconjp2024/layouts/cover.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="slidev-layout cover">
+    <div class="my-auto w-full relative">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+.slidev-layout.cover h1 {
+  font-family: var(--font-title-family);
+  font-size: 60px;
+  font-weight: 600;
+  line-height: 1.3;
+}
+
+.slidev-layout.cover h2 {
+  font-family: var(--font-title-family);
+  font-size: 36px;
+  font-weight: 400;
+  line-height: 1.1;
+
+  margin-top: 10px;
+}
+
+.slidev-layout.cover h3 {
+  font-family: var(--font-main-family);
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 1.3;
+
+  padding-right: 50px;
+  margin-top: 120px;
+}
+</style>
