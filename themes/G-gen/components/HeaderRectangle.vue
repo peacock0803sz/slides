f0b26047e6c5eed0c6196f855066ee368e064f5a
Peacock <me@p3ac0ck.net>
2025-01-30 18:07:16 +0900

    :sparkles: Add G-gen theme

---
+++ themes/G-gen/components/HeaderRectangle.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="rectangle"></div>
+</template>
+
+<style lang="postcss">
+.rectangle {
+  @apply bg-ggen-primary;
+  position: absolute;
+  top: 12px;
+  left: 25px;
+  height: 50px;
+  width: 15px;
+}
+</style>
