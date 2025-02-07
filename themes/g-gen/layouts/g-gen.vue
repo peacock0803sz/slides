e94a33420c67dc3dc4beba6b65b8091f22264aa3
Peacock <me@p3ac0ck.net>
2025-02-07 14:03:24 +0900

    :rewind: Revert removing g-gen theme

---
+++ themes/g-gen/layouts/g-gen.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="slidev-layout g-gen">
+    <slot />
+  </div>
+  <Footer />
+</template>
+
+<style lang="postcss">
+.slidev-layout.g-gen {
+  background-image: url("../assets/g-gen.svg");
+}
+</style>
---
+++ themes/g-gen/layouts/profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <HeaderRectangle />
+  <div class="slidev-layout profile text-ggen-black">
+    <slot name="text" />
+    <slot name="image-alt" />
+  </div>
+  <Footer />
+</template>
+
+<style lang="postcss">
+.slidev-layout.profile>img {
+  position: absolute;
+  top: 70px;
+  right: 50px;
+  width: 150px;
+  height: 150px;
+  float: right;
+  border-radius: 15%;
+}
+</style>
