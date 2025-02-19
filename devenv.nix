a19bbf32757930c66e38f221b4552a95d7366a1c
Peacock <me@p3ac0ck.net>
2025-02-19 15:57:56 +0900

    :snowflake: Update devenv

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env.GREET = "slides";
 
   # https://devenv.sh/packages/
   packages = with pkgs; [
@@ -34,11 +34,9 @@
   # };
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-  '';
 
-  enterShell = ''
-  '';
+  # enterShell = ''
+  # '';
 
   # https://devenv.sh/tasks/
   tasks = {
@@ -47,10 +45,10 @@
   };
 
   # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
