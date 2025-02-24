2ba2e4a60160d5986846b58b6e16512caaefe2ad
Peacock <me@p3ac0ck.net>
2025-02-24 23:42:42 +0900

    :chart_with_upwards_trend: Enable Google Analytics

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { VitePluginRadar } from 'vite-plugin-radar'
+
+export default {
+  plugins: [
+    VitePluginRadar({
+      analytics: {
+        id: 'G-VYX5T0N76K',
+      },
+    })
+  ],
+}
