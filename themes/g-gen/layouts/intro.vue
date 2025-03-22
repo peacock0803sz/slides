84ca7c5514963b17da98803b4e7fefcf35fcff3f
Peacock <me@p3ac0ck.net>
2025-03-22 17:52:40 +0900

    :sparkles: Generate QR Code for per-talk dynamiccally

---
+++ themes/g-gen/layouts/intro.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="slidev-layout intro override">
+    <slot />
+  </div>
+  <Footer />
+</template>
+
+<style>
+.slidev-layout.intro {
+  @apply text-ggen-gray1;
+  padding-top: 50px;
+  padding-left: 80px;
+  padding-right: 80px;
+}
+
+.slidev-layout.intro h1 {
+  font-size: 36pt;
+  font-weight: 600;
+  margin-bottom: 50px;
+}
+
+.slidev-layout.intro h2 {
+  font-size: 36pt;
+  font-weight: 500;
+  margin-top: 50px;
+}
+
+.slidev-layout.intro h2>svg {
+  width: 64px;
+  height: 64px;
+}
+
+.slidev-layout.intro h3 {
+  font-size: 28pt;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+</style>
