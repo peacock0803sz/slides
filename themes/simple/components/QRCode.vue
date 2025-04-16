e9f55595143ce304864e9f3e6d5213afb13d346d
Peacock <me@p3ac0ck.net>
2025-04-16 20:14:01 +0900

    :zap: Copy QRCode component from themes/g-gen

---
+++ themes/simple/components/QRCode.vue
@@ -0,0 +1,30 @@
+<template>
+  <canvas height="250" width="250" ref="canvasRef"></canvas>
+</template>
+
+<script setup lang="ts">
+import * as QRCode from "qrcode";
+import { ref, onMounted } from "vue";
+
+const props = defineProps<{ text: string }>();
+const canvasRef = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+  if (canvasRef.value) {
+    QRCode.toCanvas(canvasRef.value, props.text, function (error) {
+      if (error) console.error(error);
+    });
+  }
+});
+</script>
+
+<style>
+.slidev-layout canvas {
+  position: absolute;
+  right: 75px;
+  bottom: 75px;
+  float: right;
+  width: 250px !important;
+  height: 250px !important;
+}
+</style>
