9d5128eadf7cb8e6a14a2c2497823c5fe166b170
Peacock <me@p3ac0ck.net>
2025-09-02 18:22:53 +0900

    :sparkles: Update ll2025

---
+++ talks/2025-09-06_ll2025/layouts/sectionImg.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="slidev-layout sectionImg">
+    <slot />
+  </div>
+</template>
+
+<style>
+.slidev-layout.sectionImg {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    font-weight: 700;
+    font-size: 60px;
+  }
+
+  h2 {
+    font-weight: 400;
+    font-size: 32px;
+  }
+
+  .box {
+    display: grid;
+    grid-template-columns: 250px 600px;
+    column-gap: 25px;
+  }
+
+  .inner {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    grid-column: 2;
+    grid-row: 1;
+  }
+
+  img {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    grid-column: 1;
+    grid-row: 1;
+    width: 250px;
+  }
+
+  img.rye {
+    background-color: #333;
+    padding: 10px;
+    border-radius: 50%;
+  }
+}
+</style>
